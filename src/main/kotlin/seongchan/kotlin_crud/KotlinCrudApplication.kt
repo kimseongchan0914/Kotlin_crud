f5f01a3f9b44976977b2a60882bdc8225b8add56
kimseongchan0914 <s26014@gsm.hs.kr>
2026-08-31 20:19:11 +0900

    기본 세팅

---
+++ src/main/kotlin/seongchan/kotlin_crud/KotlinCrudApplication.kt
@@ -0,0 +1,11 @@
+package seongchan.kotlin_crud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinCrudApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinCrudApplication>(*args)
+}
